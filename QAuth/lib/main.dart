a01f51a4602a24f7118ee61d065828e4e5bb1f3d
loafman <kangjun1120@hotmail.com>
2024-03-23 09:47:34 +0800

    init

--- QAuth/lib/main.dart
+++ QAuth/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(GetMaterialApp(home: MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
